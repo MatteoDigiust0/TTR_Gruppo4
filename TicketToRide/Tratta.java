9cc2f23d11065404746b4e68a3ead7ad7bf8ec32
Matteo Digiusto <matteo.digiusto@isitgo.it>
2022-10-19 10:59:18 +0200

    .

--- TicketToRide/Tratta.java
+++ TicketToRide/Tratta.java
@@ -6,7 +6,7 @@ public class Tratta {
     String colore;
     Stazione inizio;
     Stazione fine;
-    Boolean Stazione occupata;
+    Boolean StazioneOccupata;
 
     public Tratta(int lunghezza){   //lunghezza
         this.lunghezza=lunghezza;

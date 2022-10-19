04679fc6eb90d17859cd590026a62e2df155d88f
pietromartincigh <pietro.martincigh@isitgo.it>
2022-10-19 10:05:01 +0200

    19/10 Martincigh
    Sviluppo classi Carte e inizio mazzo

---
+++ TicketToRide/Mazzo.java
@@ -0,0 +1,25 @@
+package TicketToRide;
+
+public class Mazzo{
+
+
+    private int tipo;
+    
+    
+
+    public Mazzo(){
+
+    }
+
+    public Mazzo(int tipo){
+
+        if(tipo==1){
+            
+            CartaObbiettivo obbiettivo[]=new CartaObbiettivo[30];
+            for(int i=0; i<30; i++)
+                obbiettivo=
+
+        }
+    }
+
+}

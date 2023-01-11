95f34f86765a617b01bb0939126888f1c0d35421
Matteo Digiusto <matteo.digiusto@isitgo.it>
2023-01-11 09:16:12 +0100

    ContenutoGrafico

--- TicketToRide/ContenutoGrafico.java
+++ TicketToRide/ContenutoGrafico.java
@@ -8,6 +8,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
@@ -27,6 +31,8 @@ public class ContenutoGrafico extends JPanel implements ActionListener{
         setBackground(Color.black);
         setFocusable(true);
 
+        setSize(1100, 749);
+        setLocation(230, 0);
         //spaceShip = new SpaceShip();
 
         timer = new Timer(DELAY, this);
@@ -40,27 +46,42 @@ public class ContenutoGrafico extends JPanel implements ActionListener{
         doDrawing(g);
 
         Toolkit.getDefaultToolkit().sync();
+
     }
 
     private void doDrawing(Graphics g) {
 
         Graphics2D g2d = (Graphics2D) g;
 
+        g2d.drawImage(loadImage(), 0, 0, this);
+
         //disegna tutte le immagini delle carte treno in possesso del giocatore
         for(int i=0; i<MainUI.getGiocatore().getCartatreno().length; i++ )
-        g2d.drawImage(MainUI.getGiocatore().getCartatreno()[i].getImage(), MainUI.getGiocatore().getCartatreno()[i].getX(),
-        MainUI.getGiocatore().getCartatreno()[i].getY(), this);
+            g2d.drawImage(MainUI.getGiocatore().getCartatreno()[i].getImage(), MainUI.getGiocatore().getCartatreno()[i].getX(), MainUI.getGiocatore().getCartatreno()[i].getY(), this);
 
         //disegna tutte le carte obiettivo del giocatore
         for(int i=0; i<MainUI.getGiocatore().getCarteObiettivo().length; i++ )
-        g2d.drawImage(MainUI.getGiocatore().getCarteObiettivo()[i].getImage(), MainUI.getGiocatore().getCarteObiettivo()[i].getX(),
-        MainUI.getGiocatore().getCarteObiettivo()[i].getY(), this);
+            g2d.drawImage(MainUI.getGiocatore().getCarteObiettivo()[i].getImage(), MainUI.getGiocatore().getCarteObiettivo()[i].getX(), MainUI.getGiocatore().getCarteObiettivo()[i].getY(), this);
 
         //disegna il trenino del giocatore
-       g2d.drawImage(MainUI.getGiocatore().getTreniniDisponibili()[0].getImage(), MainUI.getGiocatore().getTreniniDisponibili()[0].getX(), MainUI.getGiocatore().getTreniniDisponibili()[0].getY(), this);
+        g2d.drawImage(MainUI.getGiocatore().getTreniniDisponibili()[0].getImage(), MainUI.getGiocatore().getTreniniDisponibili()[0].getX(), MainUI.getGiocatore().getTreniniDisponibili()[0].getY(), this);
+
     }
 
+    private BufferedImage loadImage(){
 
+        URL imagePath = getClass().getResource("TTR_img/board.png");
+        BufferedImage result = null;
+
+        try{
+            result = ImageIO.read(imagePath);
+        }catch(IOException e){
+            System.err.println("Errore, immagine non trovata");
+        }
+
+        return result;
+
+    }
 
 
     @Override
--- TicketToRide/MainUI.java
+++ TicketToRide/MainUI.java
@@ -1,20 +1,28 @@
 
 package TicketToRide;
 
+import javax.swing.*;
 import java.util.*;
 
-public class MainUI {
+public class MainUI extends JFrame {
 
-    private static Giocatore giocatore = new Giocatore();
+    private ContenutoGrafico cg;
+    private Tabellone tabellone;
+    private static Giocatore giocatore;
 
     public static Giocatore getGiocatore(){
         return giocatore;
     }
 
+    public MainUI(){
 
+        tabellone = new Tabellone(25,0);
+        giocatore = new Giocatore();
 
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
 
-    public static void main(String[] args) {
+        cg = new ContenutoGrafico();
+        getContentPane().add(cg);
+    }
 
-}
 }
